039a0739f4f31d4088abf814410bd6fd5b9aad1e
James Bullin <=>
2020-09-25 14:21:51 +0100

    automate token requests

---
+++ TokenAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TokenAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TokenAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f8dc1388-1d3b-4843-bc5a-70c36414029c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TokenAPI/Tests/TokenTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TokenAPI
+{
+    [TestFixture]
+    public class TokenTests
+    {
+        // Instance of FLService
+        TokenService _lastToken = new TokenService();
+
+        [Test]
+        public void IsStateApproved()
+        {
+            Assert.That(_lastToken.LastToken.state.ToString(), Is.EqualTo("approved"));
+        }
+
+        [Test]
+        public void IsState200()
+        {
+            Assert.That(_lastToken.TokenCallManager.GetStatusCode(), Is.EqualTo("OK"));
+        }
+
+    }
+}
