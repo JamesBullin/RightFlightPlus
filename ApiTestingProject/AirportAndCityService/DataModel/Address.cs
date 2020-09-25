d2474780e21b8a533aba9c4d31f13ca7dd32ff69
Fazal Cheema <fazal.cheema@live.co.uk>
2020-09-25 11:51:31 +0100

    Created data model for airport and city search

--- ApiTestingProject/AirportAndCityService/DataModel/Address.cs
+++ ApiTestingProject/AirportAndCityService/DataModel/Address.cs
@@ -1,9 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ApiTestingProject
 {
     public class Address
     {
+        [JsonProperty("cityName")]
+        public string CityName { get; set; }
+
+        [JsonProperty("cityCode")]
+        public string CityCode { get; set; }
+
+        [JsonProperty("countryName")]
+        public string CountryName { get; set; }
+
+        [JsonProperty("countryCode")]
+        public string CountryCode { get; set; }
+
+        [JsonProperty("regionCode")]
+        public string RegionCode { get; set; }
     }
 }
--- ApiTestingProject/AirportAndCityService/DataModel/Airport.cs
+++ ApiTestingProject/AirportAndCityService/DataModel/Airport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace ApiTestingProject
 {
+    public enum LocationType { Airport, City }
+
     public class Airport
     {
+        [JsonProperty("subType")]
+        public LocationType SubType { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("detailedName")]
+        public string DetailedName { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("timeZoneOffset")]
+        public TimeSpan TimeZoneOffset { get; set; }
+
+        [JsonProperty("iataCode")]
+        public string IataCode { get; set; }
+
+        [JsonProperty("geoCode")]
+        public GeoCode GeoCode { get; set; }
+
+        [JsonProperty("address")]
+        public Address Address { get; set; }
     }
 }
