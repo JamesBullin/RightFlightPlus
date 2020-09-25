14043d5d0829fa73818b1c86f6e3b7e7b2f4f716
Fazal Cheema <fazal.cheema@live.co.uk>
2020-09-25 12:06:15 +0100

    Added test stubs

---
+++ ApiTestingProject/Tests/AirportAndCityTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ApiTestingProject
+{
+    [TestFixture]
+    public class AirportAndCityTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NumberOfResultsIsCorrect()
+        {
+        }
+
+        [Test]
+        public void NameIsCorrect()
+        {
+        }
+
+        [Test]
+        public void DetailedNameIsCorrect()
+        {
+        }
+
+        [Test]
+        public void IdIsCorrect()
+        {
+        }
+
+        [Test]
+        public void TimeZoneOffsetIsCorrect()
+        {
+        }
+
+        [Test]
+        public void GeoCodeIsCorrect()
+        {
+        }
+
+        [Test]
+        public void CityIsCorrect()
+        {
+        }
+
+        [Test]
+        public void CountryIsCorrect()
+        {
+        }
+
+        [Test]
+        public void RegionIsCorrect()
+        {
+        }
+    }
+}
