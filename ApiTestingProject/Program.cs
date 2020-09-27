bb0fa4446fa04abeab503f5400cd10349dd38751
Fazal Cheema <fazal.cheema@live.co.uk>
2020-09-27 21:11:48 +0100

    Added NUnitLite functionality

---
+++ ApiTestingProject/Program.cs
@@ -0,0 +1,18 @@
+using NUnitLite;
+
+namespace ApiTestingProject
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main program executes the tests. Output may be routed to
+        /// various locations, depending on the arguments passed.
+        /// </summary>
+        /// <remarks>Run with --help for a full list of arguments supported</remarks>
+        /// <param name="args"></param>
+        public static int Main(string[] args)
+        {
+            return new AutoRun().Execute(args);
+        }
+    }
+}
